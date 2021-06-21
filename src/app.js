6e87431437ce2a841846e98587b983673ea89d22
Sheika1404 <rosheika1404@gmail.com>
2021-06-21 03:21:52 -0400

    initial commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import '../style/styles.scss';
+window.onload = () => {
+    document.querySelector('.card').classList.add(generateSuite());
+    document.querySelector('.middle').innerHTML= generateDeck();
+    
+}
+
+
+///generate Deck 
+let generateDeck = () => {
+    const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    let getRandomNumber = (max) => Math.floor(Math.random() * max); // function to generate random number
+    return values[getRandomNumber(values.length)];
+};
+
+generateDeck();
+
+
+let generateSuite = () => {
+    const suite = ["hearts", "diamonds", "clubs", "spades"]
+    let getRandomNumber = (max) => Math.floor(Math.random() * max); // function to generate random number
+    return suite[getRandomNumber(suite.length)];
+}
+generateSuite();
+
+//   const suite = ["♥", "♦", "♠", "♣"]
+alert(window.onload);
